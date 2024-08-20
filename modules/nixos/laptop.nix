885c7a594b1858019a55b68083018e9c281ff165
Cameron Stevenson <cksteve@protonmail.com>
2024-08-20 14:56:18 +0200

    powersaving and nixvim surround

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -9,22 +9,7 @@
       iwd.enable = true;
     };
 
-    #services.thermald.enable = true;
-
-#    services.auto-cpufreq = {
-#      enable = true;
-#
-#      settings = {
-#        battery = {
-#           governor = "powersave";
-#           turbo = "never";
-#        };
-#        charger = {
-#           governor = "performance";
-#           turbo = "auto";
-#        };
-#      };
-#    };
+    services.thermald.enable = true;
   };
 }
 
